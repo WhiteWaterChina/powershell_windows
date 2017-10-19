8a9b6005b3ddc55cd394ed2421cfaa744c39bf09
Ward Yan <yanshuo5091@163.com>
2017-10-19 16:33:01 +0800

    20171019

--- monitor_broadcom_raid_temp/monitor_raid_temp_windows.ps1
+++ monitor_broadcom_raid_temp/monitor_raid_temp_windows.ps1
@@ -17,6 +17,8 @@ $null=New-Item -Name $current_date -ItemType Directory
 $log_dir=Resolve-Path $current_date|Select-Object -ExpandProperty path
 
 $start_time=Get-Date
+$status_bbu_temp=(.\storcli64.exe /c0/cv show all|Out-String) -match "Status = (.*)"
+$status_bbu=$Matches[1]
 while (0 -ne 1)
 {
 $current_time=Get-Date
@@ -30,10 +32,13 @@ else
 {
 $chip_temp_temp=((.\storcli64.exe /c0 show all)|Out-String) -match "ROC\s+.*?(\d+)"
 $chip_temp=$Matches[1]
+echo "$lasttime_temp,$chip_temp"|Out-File -Append -Force -Encoding ascii "$log_dir\chip_temp"
+if ($status_bbu -eq "Success")
+{
 $bbu_temp_temp=((.\storcli64.exe /c0/cv show all)|Out-String) -match "Temperature\s+(\d+\d*)\s*C"
 $bbu_temp=$Matches[1]
 echo "$lasttime_temp,$bbu_temp"|Out-File -Append -Force -Encoding ascii "$log_dir\bbu_temp"
-echo "$lasttime_temp,$chip_temp"|Out-File -Append -Force -Encoding ascii "$log_dir\chip_temp"
+}
 sleep 5
 }
 }
@@ -51,6 +56,8 @@ $max_chip_temp=$result_data_chip.Maximum
 $min_chip_temp=$result_data_chip.Minimum
 
 #filter data for bbu
+if ($status_bbu -eq "Success")
+{
 $data_bbu_temp=Get-Content "$log_dir\bbu_temp"
 $length_bbu=$data_bbu_temp.Length
 for ($item=0;$item -lt $length_bbu;$item++)
@@ -61,8 +68,11 @@ $data_bbu=Get-Content "$log_dir\bbu_temp_temp"
 $result_data_bbu=$data_bbu|Measure-Object -Minimum -Maximum
 $max_bbu_temp=$result_data_bbu.Maximum
 $min_bbu_temp=$result_data_bbu.Minimum
+}
 
 #filter data to plot in one picture
+if ($status_bbu -eq "Success")
+{
 if ($max_chip_temp -gt $max_bbu_temp)
 {
 $max_temp=$max_chip_temp
@@ -71,7 +81,14 @@ else
 {
 $max_temp=$max_bbu_temp
 }
+}
+else
+{
+$max_temp=$max_chip_temp
+}
 
+if ($status_bbu -eq "Success")
+{
 if ($min_chip_temp -lt $min_bbu_temp)
 {
 $min_temp=$min_chip_temp
@@ -80,11 +97,17 @@ else
 {
 $min_temp=$min_bbu_temp
 }
-
+}
+else
+{
+$min_temp=$min_chip_temp
+}
 $max_temperature=[int]$max_temp + 2
 $min_temperature=[int]$min_temp - 2
 
 #plot image
+if ($status_bbu -eq "Success")
+{
 $scripts=@"
 set terminal png
 set title 'Temperature'
@@ -99,6 +122,24 @@ set key box
 plot '${log_dir}\bbu_temp' title 'BBU Temp' w lp pt 5, '${log_dir}\chip_temp'  title 'Chip Temp' w lp pt 7 
 set output
 "@
+}
+else
+{
+$scripts=@"
+set terminal png
+set title 'Temperature'
+set datafile separator ','
+set xrange [0:${lasttime_temp}]
+set yrange [${min_temperature}:${max_temperature}]
+set ylabel 'Temperature(Celsius)'
+set xlabel 'Time(s)'
+set output "Image_temperature.png"
+set border 3 lt 3 lw 2
+set key box
+plot '${log_dir}\chip_temp'  title 'Chip Temp' w lp pt 7 
+set output
+"@
+}
 [System.IO.File]::WriteAllLines("${log_dir}\gnuplot.txt",$scripts, (New-Object System.Text.UTF8Encoding $False))
 .\bin\gnuplot.exe $log_dir\gnuplot.txt
 Move-Item Image_temperature.png -Destination $log_dir
